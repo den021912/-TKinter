22e8219f16de30ca5e86cd8fa779d85b615b9dad
Денис <usakovdenis440@gmail.com>
2024-11-21 19:17:33 +0400

    Update drawing_app.py

--- drawing_app.py
+++ drawing_app.py
@@ -7,13 +7,18 @@ class DrawingApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Рисовалка с сохранением в PNG")
+
         self.image = Image.new("RGB", (600, 400), "white")
         self.draw = ImageDraw.Draw(self.image)
+
         self.canvas = tk.Canvas(root, width=600, height=400, bg='white')
         self.canvas.pack()
+
         self.setup_ui()
+
         self.last_x, self.last_y = None, None
         self.pen_color = 'black'
+
         self.canvas.bind('<B1-Motion>', self.paint)
         self.canvas.bind('<ButtonRelease-1>', self.reset)
 
@@ -21,47 +26,46 @@ class DrawingApp:
     def setup_ui(self):
         control_frame = tk.Frame(self.root)
         control_frame.pack(fill=tk.X)
+
         clear_button = tk.Button(control_frame, text="Очистить", command=self.clear_canvas)
         clear_button.pack(side=tk.LEFT)
+
         color_button = tk.Button(control_frame, text="Выбрать цвет", command=self.choose_color)
         color_button.pack(side=tk.LEFT)
+
         save_button = tk.Button(control_frame, text="Сохранить", command=self.save_image)
         save_button.pack(side=tk.LEFT)
-        self.brush_size_scale = tk.Scale(control_frame, from_=1, to=10, orient=tk.HORIZONTAL)
-        self.brush_size_scale.pack(side=tk.LEFT)
+        
         """Выпадающий список для выбора размера кисти"""
-        label = ttk.Label(control_frame, text='Размер кисти:') # создание кнопки для выбора размера кисти
-        label.pack(side=tk.LEFT)
         sizes = ['1', '2', '5', '10']
-        combo = ttk.OptionMenu(control_frame, self.brush_size_scale, sizes[0], *sizes)
-        combo.pack(side=tk.LEFT)
+        self.brush_size = tk.IntVar(control_frame)
+        self.brush_size.set(sizes[0])
+        brush_size_menu = ttk.OptionMenu(control_frame, self.brush_size, *sizes, command = self.update_brush_size)
+        brush_size_menu.pack(side=tk.LEFT)
+       
 
-  
     def paint(self, event):
         if self.last_x and self.last_y:
             self.canvas.create_line(self.last_x, self.last_y, event.x, event.y,
-                                    width=self.brush_size_scale.get(), fill=self.pen_color,
+                                    width=self.brush_size.get(), fill=self.pen_color,
                                     capstyle=tk.ROUND, smooth=tk.TRUE)
             self.draw.line([self.last_x, self.last_y, event.x, event.y], fill=self.pen_color,
-                           width=self.brush_size_scale.get())
+                           width=self.brush_size.get())
+
         self.last_x = event.x
         self.last_y = event.y
 
-  
     def reset(self, event):
         self.last_x, self.last_y = None, None
 
-  
     def clear_canvas(self):
         self.canvas.delete("all")
         self.image = Image.new("RGB", (600, 400), "white")
         self.draw = ImageDraw.Draw(self.image)
 
-  
     def choose_color(self):
         self.pen_color = colorchooser.askcolor(color=self.pen_color)[1]
 
-  
     def save_image(self):
         file_path = filedialog.asksaveasfilename(filetypes=[('PNG files', '*.png')])
         if file_path:
@@ -70,6 +74,8 @@ class DrawingApp:
             self.image.save(file_path)
             messagebox.showinfo("Информация", "Изображение успешно сохранено!")
 
+    def update_brush_size(self, size):
+        self.brush_size = int(size)
 
 def main():
     root = tk.Tk()
